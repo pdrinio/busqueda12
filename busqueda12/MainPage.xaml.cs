d278d4bd2a8e60f4224dbd2ba07ddf31cc03ea75
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-05-11 18:46:30 +0200

    V1

--- busqueda12/MainPage.xaml.cs
+++ busqueda12/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml.Media.Imaging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +25,52 @@ namespace busqueda12
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SpeechSynthesizer synthesizer;
+
         public MainPage()
         {
             this.InitializeComponent();
+            InicializaHabla();
         }
+
+        private async void BtnEmpieza_Click(object sender, RoutedEventArgs e)
+        {
+            await Dime("Hola, Cris; soy Leidi Bag. Te voy a ayudar a encontrar el código secreto de Joc Moz. Lo tiene Alya, y para encontrarla tienes que saber que le encantan los libros de mayores. ¿Dónde estará?");                     
+        }
+
+        private void InicializaHabla()
+        {
+            //lanza el habla
+            synthesizer = new SpeechSynthesizer();
+
+            VoiceInformation voiceInfo =
+         (
+           from voice in SpeechSynthesizer.AllVoices
+           where voice.Gender == VoiceGender.Female
+           select voice
+         ).FirstOrDefault() ?? SpeechSynthesizer.DefaultVoice;
+
+            synthesizer.Voice = voiceInfo;
+        }
+
+        private async Task Dime(String szTexto)
+        {
+            try
+            {
+                // crear el flujo desde el texto
+                SpeechSynthesisStream synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(szTexto);
+
+                // ...y lo dice
+                media.AutoPlay = true;
+                media.SetSource(synthesisStream, synthesisStream.ContentType);
+                media.Play();
+            }
+            catch (Exception e)
+            {
+                var msg = new Windows.UI.Popups.MessageDialog(e.Message, "Error hablando:");
+                await msg.ShowAsync();
+            }
+        }
+     
     }
 }
